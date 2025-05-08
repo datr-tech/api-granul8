8ac70e3786d34e70dc6b3984facf22e0ff76faf3
joealdersonstrachan <joealdersonstrachan@strachan.email>
2025-05-08 02:21:35 +0100

    feat: update docker config and add the 'leith common seeders' dep

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,39 @@
-import { app } from '@app-ag/api';
-import { apiName, apiPort, dbHost, dbName, dbPort } from '@app-ag/config';
-import { logger } from '@datr.tech/leith-common-logger';
-import { db } from '@datr.tech/leith-common-mongodb-connector';
 import 'dotenv/config';
 
+import { logger } from '@datr.tech/leith-common-logger';
+import { db } from '@datr.tech/leith-common-mongodb-connector';
+import { granul8Seeder } from '@datr.tech/leith-common-seeders';
+
+import { app } from '@app-ag/api';
+import {
+  AttributeModel,
+  AttributeTypeModel,
+  ResourceAttributeModel,
+  UserAttributeModel,
+} from '@app-ag/api/models';
+import { apiName, apiPort, dbHost, dbName, dbPort } from '@app-ag/config';
+
 app.listen(apiPort, () => {
-  logger.info(`${apiName} listening on ${apiPort}`);
+  logger.info(`api-${apiName} listening on ${apiPort}`);
 
   (async () => {
-    await db.connect({
+    const stat = await db.connect({
       host: dbHost,
       name: dbName,
       port: dbPort,
       user: undefined,
       pass: undefined,
     });
+
+    const { isConnected } = stat;
+
+    if (isConnected) {
+      await granul8Seeder(
+        AttributeModel,
+        AttributeTypeModel,
+        ResourceAttributeModel,
+        UserAttributeModel,
+      );
+    }
   })();
 });
